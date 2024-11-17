7c0206969297090eda823cf7e3bf8a047ad78a49
Dinhero21 <dinhero21@dinhero21.dev>
2024-11-16 23:36:48 -0300

    funny files

---
+++ README.md
@@ -0,0 +1,6 @@
+# prismarine.entity
+
+- [prismarine-entity](https://github.com/prismarinejs/prismarine-entity) wrapper
+
+This is an [MMP](https://github.com/DinheroDevelopmentGroup/modular-minecraft-proxy) module.
+Generated using [M3](https://github.com/DinheroDevelopmentGroup/modular-minecraft-proxy/tree/main/src/m3) init.
---
+++ local.ts
@@ -0,0 +1,12 @@
+import type { Entity as TEntity } from 'prismarine-entity';
+import factory from 'prismarine-entity';
+
+import registry from '../prismarine.registry/local.js';
+
+export type Entity = TEntity;
+
+export const Entity: typeof TEntity = (
+  factory as unknown as (
+    registryOrVersion: typeof registry | string,
+  ) => typeof TEntity
+)(registry);
